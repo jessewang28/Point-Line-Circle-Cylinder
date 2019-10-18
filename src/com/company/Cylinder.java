525cc9bc21588260a14a8cba907ac4e209a74fa1
student <jessewang@billericak12.com>
2019-10-18 12:36:27 -0400

    comit

--- src/com/company/Cylinder.java
+++ src/com/company/Cylinder.java
@@ -4,10 +4,15 @@ public class Cylinder {
     private Circle c1;
     private double h;
 
+    public Cylinder(int r,double h)
+    {
+        c1 = new Circle(r);
+        double height = h;
+    }
+
     public Cylinder()
     {
-
-
-
+        c1 = new Circle();
+        double h = 1;
     }
 }
